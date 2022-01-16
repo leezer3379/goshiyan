aaef042495ae52ed51af9ed3f61bb28993c82d81
Your Name <you@example.com>
2022-01-16 13:26:48 +0800

    update

--- main.go
+++ main.go
@@ -8,94 +8,93 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"time"
 )
 
 type User struct {
 	gorm.Model
-	Name      string `gorm:"type:varchar(20);not null"`
-	Telephone string `gorm:"type:varchar(11);not null;unique"`
-	Password  string `gorm:"size:255;not null"`
+	Name string `gorm:"type:varchar(20);not null"`
+	Telephone string `gorm:"varchar(11);not null;unique"`
+	Password string `gorm:"size:255;not null"`
+
 }
 
 func isTelephoneExist(db *gorm.DB, telephone string) bool {
 	var user User
 	db.Where("telephone = ?", telephone).First(&user)
+
 	if user.ID != 0 {
+		// 存在
 		return true
 	}
+
 	return false
 }
 
-func main() {
-	// init db
-	db := initDB()
+func main()  {
+	db := InitDB()
+	// 1.创建路由
 	r := gin.Default()
-	r.POST("/api/auth/register", func(c *gin.Context) {
-		// 获取参数
-		name := c.PostForm("name")
-		telephone := c.PostForm("telephone")
-		password := c.PostForm("password")
+	// 2.绑定路由规则，执行的函数
+	// gin.Context，封装了request和response
+	r.POST("/api/auth/register", func(ctx *gin.Context) {
+		name := ctx.PostForm("name")
+		telephone := ctx.PostForm("telephone")
+		password := ctx.PostForm("password")
 
-		// 数据验证
 		if len(telephone) != 11 {
-			fmt.Println(len(telephone))
-			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须位11位"})
 			return
 		}
-		//验证密码
 		if len(password) < 6 {
-			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于6位"})
 			return
 		}
 
-		// 如果名称没有传，给一个10位数随机数
 		if len(name) == 0 {
 			name = RandomString(10)
-
 		}
 
-		log.Println(name, telephone, password)
-
-		// 判断手机号是否存在
+		log.Println(name,telephone, password)
+		//  查询
 		if isTelephoneExist(db, telephone) {
-			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已存在"})
+			ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户已存在"})
 			return
 		}
-
-		// 创建用户
-		newUser := User{
-			Name:      name,
+		// 新建
+		db.Create(&User{
+			Name: name,
 			Telephone: telephone,
-			Password:  password,
-		}
-
-		db.Create(&newUser)
-		c.JSON(200, gin.H{
-			"message": "注册成功",
+			Password: password,
 		})
-	})
-	r.Run() // 监听并在 0.0.0.0:8080 上启动服务
-}
 
+
+
+		ctx.JSON(http.StatusOK, gin.H{"code": 200, "msg": "注册成功"})
+	})
+	// 3.监听端口，默认在8080
+	// Run("里面不指定端口号默认为8080")
+	r.Run(":8000")
+	fmt.Println("work")
+
+}
 func RandomString(n int) string {
 	var letters = []byte("asdfghjklzxcvbnmqwertyuiopASDFGHJKLZXCVBNMQWERTYUIOP")
 	result := make([]byte, n)
-
-	rand.Seed(time.Now().Unix())
 	for i := range result {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
+
 	return string(result)
 }
 
-//
-func initDB() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+func InitDB() *gorm.DB {
+	// github.com/mattn/go-sqlite3
+	db, err := gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 	if err != nil {
-		panic("数据库连接失败")
+		panic(err)
 	}
 
 	db.AutoMigrate(&User{})
 	return db
+
 }
